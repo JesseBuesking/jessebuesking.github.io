341fc43c10c16833f6e9edba20b649637f1b0f15
JesseBuesking <jessebuesking@gmail.com>
2014-06-07 12:23:22 -0700

    compression tools plus updating scripts.min.js

---
+++ assets/compression-tools/compile-js.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "cd into the projects compression-tools directory before running"
+
+dirname=../js
+compiler=compiler.jar
+tmpfile=./tmp.js
+
+if [ -e $tmpfile ]; then rm $tmpfile; fi
+
+# getting plugins
+files=`find $dirname/plugins -name "*.js"`
+for i in $files;
+do
+    echo "adding file $i"
+    cat $i >> $tmpfile;
+done
+
+# getting _main.js
+echo "adding file $dirname/_main.js"
+cat $dirname/_main.js >> $tmpfile
+
+java -jar $compiler $tmpfile --compilation_level ADVANCED_OPTIMIZATIONS --js_output_file $dirname/scripts.min.js --warning_level QUIET --summary_detail_level 3 && rm $tmpfile || echo 'failed to compress js files'
